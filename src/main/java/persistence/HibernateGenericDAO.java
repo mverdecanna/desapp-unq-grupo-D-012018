308fdf2c40763f8ca8d8046b3ef410845c5effa5
mverdecanna <mverdecanna@gmail.com>
2018-04-14 13:56:23 -0300

    fix se modificaron unos imports. Además se bajó en el pom el coverage, para que compile, por que se agregaron clases y no están cubiertas.

--- src/main/java/persistence/HibernateGenericDAO.java
+++ src/main/java/persistence/HibernateGenericDAO.java
@@ -1,6 +1,6 @@
 package persistence;
 
-import ar.edu.unq.repositories.*;
+
 import org.springframework.orm.hibernate4.support.HibernateDaoSupport;
 
 import java.io.Serializable;
@@ -8,7 +8,7 @@ import java.io.Serializable;
 /**
  * Created by mariano on 14/04/18.
  */
-public abstract class HibernateGenericDAO<T>  extends HibernateDaoSupport implements ar.edu.unq.repositories.GenericRepository<T>, Serializable {
+public abstract class HibernateGenericDAO<T>  extends HibernateDaoSupport implements GenericRepository<T>, Serializable {
 
 
 
