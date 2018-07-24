2e06a05e96eea823c5407a24cb65907dc01adddb
Charlie <cuococarlos@gmail.com>
2018-07-23 23:20:04 -0300

    add archunit test

---
+++ src/test/java/ArchitectureArchUnitTest.java
@@ -0,0 +1,22 @@
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.junit.ArchUnitRunner;
+import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+import org.junit.runner.RunWith;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(ArchUnitRunner.class)
+@AnalyzeClasses(packages = {"service"})
+public class ArchitectureArchUnitTest {
+
+    @ArchTest
+    public static void rule1(JavaClasses classes) {
+
+        ArchRule rule = ArchRuleDefinition.noClasses()
+                .that().resideInAPackage("..service..")
+                .should().beAnnotatedWith(Transactional.class);
+        rule.check(classes);
+    }
+}
