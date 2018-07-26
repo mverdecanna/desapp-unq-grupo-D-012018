caa726f6a555fe3510e123fc845e12c66941a7a8
mverdecanna <mverdecanna@gmail.com>
2018-07-26 09:32:43 -0300

    se suben cambios: de modelo, se modifico el servicio de saveUser para que cree la CC cuando se registra. Se cambio el llamado del mailService para que se lo llame dentro de rentalService... no pude hacer que esto funcione en los tests cuando se compila, asi que lo deje comentado por ahora. Se agregó algo de validación en el back, hay que ver si alcanza con lo que se hizo o hay que darle una vuelta mas y combinarlo con el objeto Response.

---
+++ src/main/java/model/exceptions/InsufficientBalanceException.java
@@ -0,0 +1,13 @@
+package model.exceptions;
+
+/**
+ * Created by mariano on 26/07/18.
+ */
+public class InsufficientBalanceException extends Exception {
+
+
+    public InsufficientBalanceException(String message){
+        super(message);
+    }
+
+}
---
+++ src/main/java/model/exceptions/InvalidRegisterParameterException.java
@@ -0,0 +1,14 @@
+package model.exceptions;
+
+/**
+ * Created by mariano on 26/07/18.
+ */
+public class InvalidRegisterParameterException extends Exception {
+
+
+    public InvalidRegisterParameterException(String message){
+        super(message);
+    }
+
+
+}
